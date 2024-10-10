f6d5bc32e3c54c90cc69fa293c1a9315eec748c2
Samuel Santana <contato.santanasamuel@gmail.com>
2024-10-10 17:32:52 -0300

    first commit: criando backend da aplicação

---
+++ desafio-cnab/src/main/java/com/example/desafio_cnab/DesafioCnabApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafio_cnab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCnabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCnabApplication.class, args);
+	}
+
+}
---
+++ desafio-cnab/src/test/java/com/example/desafio_cnab/DesafioCnabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_cnab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCnabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
